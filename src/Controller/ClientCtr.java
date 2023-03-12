484bd162f51a17be01355a6b5ccfbca407f0cb87
HuynhThanhSonB1906560 <sobinson2001@gmail.com>
2023-03-12 23:55:14 +0700

    update login

--- src/Controller/ClientCtr.java
+++ src/Controller/ClientCtr.java
@@ -6,6 +6,7 @@ package Controller;
 
 import client.Message;
 import entities.HoaDon;
+import entities.NhanVien;
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.File;
@@ -19,73 +20,84 @@ import java.net.Socket;
 import java.util.ArrayList;
 
 public class ClientCtr {
+
     private int port;
     private String host;
-    private Socket clientsocket ;
-    public ClientCtr(){
-        this.port=  8888;
-        this.host="localhost";
+    private Socket clientsocket;
+
+    public ClientCtr() {
+        this.port = 8888;
+        this.host = "localhost";
     }
-    
-    public void Connect(){
+
+    public void Connect() {
         try {
-            clientsocket = new Socket(host,port);
+            clientsocket = new Socket(host, port);
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
-    
-    public void SendMes(String mes){
+
+    public void SendMes(String mes) {
         try {
-            PrintWriter pr =new PrintWriter(clientsocket.getOutputStream());
+            PrintWriter pr = new PrintWriter(clientsocket.getOutputStream());
             pr.println(mes);
             pr.flush();
         } catch (Exception e) {
         }
     }
-    
-    public String ResultMes() throws IOException{
-            String Message="";
-            InputStreamReader in = new InputStreamReader(clientsocket.getInputStream());
-            BufferedReader bf = new BufferedReader(in);
-            Message=(String)bf.readLine();
-     return Message;
+
+    public String ResultMes() throws IOException {
+        String Message = "";
+        InputStreamReader in = new InputStreamReader(clientsocket.getInputStream());
+        BufferedReader bf = new BufferedReader(in);
+        Message = (String) bf.readLine();
+        return Message;
     }
-    
-    public void SaveBill(String mes,HoaDon bill) throws IOException{
+
+    public void SaveBill(String mes, HoaDon bill) throws IOException {
         try {
-            ObjectOutputStream oos=new ObjectOutputStream(clientsocket.getOutputStream()) ;
+            ObjectOutputStream oos = new ObjectOutputStream(clientsocket.getOutputStream());
             oos.writeObject(mes);
             oos.flush();
             oos.writeObject(bill);
         } catch (Exception e) {
             e.printStackTrace();
-        }         
+        }
     }
-    public void SearchNameProduct(String mes, String search){
+
+    public void SearchNameProduct(String mes, String search) {
         try {
-            ObjectOutputStream oos=new ObjectOutputStream(clientsocket.getOutputStream()) ;
+            ObjectOutputStream oos = new ObjectOutputStream(clientsocket.getOutputStream());
             oos.writeObject(mes);
             oos.flush();
             oos.writeObject(search);
         } catch (Exception e) {
             e.printStackTrace();
-        }    
+        }
     }
-    
-    public ArrayList<String> ResultNameProduct () throws IOException, ClassNotFoundException{
+
+    public ArrayList<String> ResultNameProduct() throws IOException, ClassNotFoundException {
         ObjectInputStream ois = new ObjectInputStream(clientsocket.getInputStream());
-        ArrayList<String> list =(ArrayList<String>) ois.readObject();
+        ArrayList<String> list = (ArrayList<String>) ois.readObject();
         return list;
     }
-    
-    
-    
-    public void closeConnect(){
+
+    public void Login(String mes) throws IOException {
+        try {
+            ObjectOutputStream oos = new ObjectOutputStream(clientsocket.getOutputStream());
+            oos.writeObject(mes);
+            oos.flush();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void closeConnect() {
         try {
             clientsocket.close();
         } catch (Exception e) {
-          e.printStackTrace();
+            e.printStackTrace();
         }
-        }
+    }
 }
--- src/client/Login.java
+++ src/client/Login.java
@@ -4,6 +4,14 @@
  */
 package client;
 
+import Controller.ClientCtr;
+import entities.NhanVien;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+import main.Main;
+
 /**
  *
  * @author user
@@ -32,27 +40,42 @@ public class Login extends javax.swing.JFrame {
         jLabel2 = new javax.swing.JLabel();
         jTextField1 = new javax.swing.JTextField();
         jLabel3 = new javax.swing.JLabel();
-        jTextField2 = new javax.swing.JTextField();
         jButton1 = new javax.swing.JButton();
         jButton2 = new javax.swing.JButton();
         jButton3 = new javax.swing.JButton();
+        jPasswordField1 = new javax.swing.JPasswordField();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setUndecorated(true);
 
         jLabel1.setFont(new java.awt.Font("Segoe UI", 0, 36)); // NOI18N
         jLabel1.setForeground(new java.awt.Color(0, 153, 51));
-        jLabel1.setText("Login");
+        jLabel1.setText("Đăng nhập");
 
-        jLabel2.setText("Email :");
+        jLabel2.setText("Tài khoản:");
 
         jLabel3.setText("Mật Khẩu :");
 
         jButton1.setText("Login");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         jButton2.setText("Clear");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
 
-        jButton3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/close (1).png"))); // NOI18N
+        jButton3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/icons8-close-20.png"))); // NOI18N
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -60,17 +83,17 @@ public class Login extends javax.swing.JFrame {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addGap(30, 30, 30)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 67, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(jLabel3, javax.swing.GroupLayout.DEFAULT_SIZE, 67, Short.MAX_VALUE)
+                    .addComponent(jLabel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addGap(38, 38, 38)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(jButton2)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 153, Short.MAX_VALUE)
                         .addComponent(jButton1))
                     .addComponent(jTextField1, javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jTextField2, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.PREFERRED_SIZE, 297, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(jPasswordField1))
                 .addContainerGap(92, Short.MAX_VALUE))
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
@@ -79,33 +102,80 @@ public class Login extends javax.swing.JFrame {
                         .addComponent(jButton3)
                         .addGap(16, 16, 16))
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                        .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 105, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(192, 192, 192))))
+                        .addComponent(jLabel1)
+                        .addGap(160, 160, 160))))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addGap(15, 15, 15)
                 .addComponent(jButton3)
-                .addGap(7, 7, 7)
+                .addGap(3, 3, 3)
                 .addComponent(jLabel1)
-                .addGap(40, 40, 40)
+                .addGap(44, 44, 44)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel2)
                     .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(46, 46, 46)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel3)
-                    .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(jPasswordField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(59, 59, 59)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jButton1)
                     .addComponent(jButton2))
-                .addContainerGap(78, Short.MAX_VALUE))
+                .addContainerGap(58, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        jTextField1.setText("");
+        jPasswordField1.setText("");
+    }//GEN-LAST:event_jButton2ActionPerformed
+
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+        System.exit(0);
+    }//GEN-LAST:event_jButton3ActionPerformed
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        try {
+            NhanVien nv = new NhanVien();
+            StringBuilder ab = new StringBuilder();
+            String username = jTextField1.getText();
+            nv.setNv_taikhoan(username);
+            String password = new String(jPasswordField1.getPassword());
+            nv.setNv_matkhau(password);
+            if (username.equals("")) {
+                ab.append("Tên người dùng trống\n");
+            }
+            if (password.equals("")) {
+                ab.append("Mật khẩu trống\n");
+            }
+            if (ab.length() > 0) {
+                JOptionPane.showMessageDialog(this, ab.toString(), "Invalidation", JOptionPane.ERROR_MESSAGE);
+                return;
+            }
+            ClientCtr ctr = new ClientCtr();
+            ctr.Connect();
+            ctr.Login("Login");
+//            ctr.SendMes(username + " " + password);
+            ctr.SendMes(username);
+            ctr.SendMes(password);
+            String ac = ctr.ResultMes();
+            if (ac.equals("Đăng nhập thành công")) {
+                JOptionPane.showMessageDialog(this, "Đăng nhập thành công");
+                Main trangchu = new Main();
+                trangchu.setVisible(true);
+            } else {
+                JOptionPane.showMessageDialog(this, "Sai tài khoản hoặc mật khẩu");
+            }
+            ctr.closeConnect();
+        } catch (IOException ex) {
+            Logger.getLogger(Login.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -149,7 +219,7 @@ public class Login extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
+    private javax.swing.JPasswordField jPasswordField1;
     private javax.swing.JTextField jTextField1;
-    private javax.swing.JTextField jTextField2;
     // End of variables declaration//GEN-END:variables
 }
